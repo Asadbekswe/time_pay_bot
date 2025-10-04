4eeaf3b350ea9dfbf656bd6887a821113e8cce72
mexmonjonovuz <asadbekmehmonjonov89@gmail.com>
2025-10-04 23:43:36 +0500

    changes

--- migrations/env.py
+++ migrations/env.py
@@ -9,7 +9,6 @@ from sqlalchemy.ext.asyncio import async_engine_from_config
 from config import conf
 from database import Base
 
-
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
